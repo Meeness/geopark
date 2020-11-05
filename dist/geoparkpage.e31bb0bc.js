8b014060b079dd810721549bdbc22d678cfb820f
Henry Moon <mheeness@gmail.com>
2020-11-05 13:37:54 +0100

    quiz page got buttons for answers

--- dist/geoparkpage.e31bb0bc.js
+++ dist/geoparkpage.e31bb0bc.js
@@ -18290,7 +18290,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61433" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52896" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
