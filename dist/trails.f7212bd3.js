20e8db6753f1f9a31d536f932f18b67a47028970
vlovkis <vlovkis@inbox.lt>
2020-11-10 02:32:57 +0100

    New pages

--- dist/trails.f7212bd3.js
+++ dist/trails.f7212bd3.js
@@ -18319,7 +18319,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49861" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63060" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
