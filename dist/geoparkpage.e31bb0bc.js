e01fc84a15bb59095eae8ce69ca348e878403a73
Henry Moon <mheeness@gmail.com>
2020-11-12 13:02:29 +0100

    school

--- dist/geoparkpage.e31bb0bc.js
+++ dist/geoparkpage.e31bb0bc.js
@@ -18290,7 +18290,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54644" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57671" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
